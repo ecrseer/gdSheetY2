e7d611b8d26f9d5ed85ebeee8f8627a835e8913b
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-04 11:45:11 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,7 @@ export default function indX2() {
         }
         
         )
-        useEffect(()=>{setTimeout(()=>{
-           
-        }
-    },[])
+        
 
         async function handleProcurai(eventoSb){
             eventoSb.preventDefault();
