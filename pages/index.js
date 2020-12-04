f5862053c65dd74de15c949d41663ff8a854976b
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-04 00:03:51 -0300

    no image

--- pages/index.js
+++ pages/index.js
@@ -28,11 +28,10 @@ export default function indX2() {
     return(
   <>
     <Head>
-      <title>next-pwa example</title>
+      <title>tabela</title>
     </Head>
-    <h1>Next.js + PWA = AWESOME!</h1>
-    <h2>Following image will fallback to placeholder when offline</h2>
-    <img src='https://source.unsplash.com/600x400/?cat' />
+    <h2>gdSheet - minha tabela excel</h2>
+    
     <div>
     <form onSubmit={handleProcurai}>
         <input placeholder="pesquisa" type="text" value={nomezin}
